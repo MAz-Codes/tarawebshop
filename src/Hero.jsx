0afdd91ad8b8ab5ed10a1829a869a48af919032a
mazcoding <115239190+mazcoding@users.noreply.github.com>
2023-02-20 14:22:30 +0100

    made the hero background a video

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,21 +1,38 @@
-import { Box, Link, Heading, Flex, Text, Button } from '@chakra-ui/react';
+import React from 'react'
+import Video from "./assets/take.mp4"
+import { Box, Link, Divider, Heading, Text, Button } from '@chakra-ui/react'
 
-const Hero = () => {
+function Hero() {
   return (
     <Box
-      position="relative"
-      bgImage="url('https://images.unsplash.com/photo-1567631643547-67a2dd59f266?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80')"
-      bgPosition="center"
-      bgRepeat="no-repeat"
-      bgSize="cover"
-      align="center"
-      borderRadius={"3xl"}
-      p={6}
-      mx={{ base: '5', lg: '20' }}
+    bgSize="cover"
+    position="relative"
+    mx={{ base: '2', lg: '1' }}
+    align="center"
+    p={6}
     >
+  <video
+    
+    autoPlay
+    muted
+    loop
+    style={{
+      borderRadius: "30px",
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      objectFit: "cover",
+      opacity: 0.5,
+      filter: "grayscale(100%)"
+    }}
+  >
+    <source src={Video} type="video/mp4"/>
 
-      <Box margin="0 auto" maxW="64rem" py={{ base: '1rem', lg: '8rem' }} >
-        <Heading as="h2" fontSize={{ base: '2.25rem', lg: '3rem' }}  color="#fff" maxW={{ base: '100%', md: '45%' }}>
+  </video>
+  <Box margin="0 auto" maxW="64rem" py={{ base: '1rem', lg: '8rem' }} position="relative" >
+        <Heading as="h2" fontSize={{ base: '2.25rem', lg: '3rem' }}  color="#1a1f2c" maxW={{ base: '100%', md: '45%' }}>
           Taking Sustainable Fashion To The Next Level.
         </Heading>
         <Box
@@ -23,7 +40,7 @@ const Hero = () => {
           width={{ base: '100%', md: '30%' }}
           mb={{ base: '1rem', lg: '0' }}
         >
-          <Text mb="0.5rem" color="#fff">
+          <Text mb="0.5rem" color="#1a1f2c">
             Find sustainable and fairtrade fashion
           </Text>
           <Button
@@ -34,16 +51,15 @@ const Hero = () => {
             px="2.5rem"
             py="1.5rem"
             width="full"
-            border="2px solid #fff"
             rounded="md"
-            _hover={{ bg: 'gray.300', textDecoration: 'none' }}
+            _hover={{ bg: 'gray.300', textColor: "#1a1f2c",  textDecoration: 'none' }}
           >
             Explore Catalog
           </Button>
         </Box>
       </Box>
-    </Box>
-  );
-};
+  </Box>
+  )
+}
 
-export default Hero;
+export default Hero
