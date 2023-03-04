fe99962f610c73b957a2f56633b49ba0e08c8165
mazcoding <115239190+mazcoding@users.noreply.github.com>
2023-03-04 09:50:27 +0100

    update

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,6 @@
 import '../App.css'
-import { Route, Routes } from "react-router-dom";
 import Hero from '../Elements/Hero'
-import Navbar from '../Elements/Navbar'
 import Highlights from '../Elements/Highlights'
-import Footer from '../Elements/Footer'
 
 function Home() {
 
